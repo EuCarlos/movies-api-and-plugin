c81617e64aebd8049026a9959acca8666f3a5cc3
Carlos Alves <carlosalves369m@hotmail.com>
2023-03-12 09:21:59 -0300

    :heavy_plus_sign: add cors dependecy

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,17 +2,25 @@
 import express from 'express'
 import { print } from './routes/functions/printPaths'
 require("dotenv").config()
+import cors from 'cors'
 
 // routes
 const v1Router = require('./routes/v1')
 
 import JsonResponse from './concerns/response'
 
+
 const PORT = process.env.PORT || 3333
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200
+}
 
 const app = express();
 
+
 app
+    .use(cors(corsOptions))
     .use('/api/v1/movies', v1Router)
 
     .use((req, res) => {
